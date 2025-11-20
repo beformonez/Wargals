9c5e4bca0b87e9b6c0ed7a8ce04bebde40722211
beformonez <beformonez@gmail.com>
2025-11-20 10:58:14 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,36 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
-   const express = require('express');
-   const http = require('http');
-   const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-   const app = express();
-   const server = http.createServer(app);
-   const io = socketIo(server);
+app.use(express.static(path.join(__dirname, 'public')));
 
-   app.use(express.static('public')); // Serve static files (HTML, JS, assets)
+let players = {};
 
-   let players = {}; // Store player data: {id: {x, y, direction}}
-   let blocks = {}; // Store placed blocks: {x_y: type}
-   let projectiles = {}; // Store projectiles: {id: {x, y, vx, vy, owner}}
+io.on('connection', (socket) => {
+  console.log('Player connected:', socket.id);
+  players[socket.id] = { x: 100, y: 100 };
 
-   io.on('connection', (socket) => {
-     console.log('Player connected:', socket.id);
+  io.emit('players', players);
 
-     // Add new player
-     players[socket.id] = { x: 100, y: 100, direction: 'right' };
-     socket.emit('init', { players, blocks, projectiles });
+  socket.on('move', (data) => {
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+      io.emit('players', players);
+    }
+  });
 
-     // Handle player movement
-     socket.on('move', (data) => {
-       players[socket.id] = data;
-       socket.broadcast.emit('updatePlayers', players);
-     });
+  socket.on('disconnect', () => {
+    delete players[socket.id];
+    io.emit('players', players);
+    console.log('Player disconnected:', socket.id);
+  });
+});
 
-     // Handle block placement
-     socket.on('placeBlock', (data) => {
-       blocks[data.key] = data.type;
-       io.emit('updateBlocks', blocks);
-     });
-
-     // Handle projectile shooting
-     socket.on('shoot', (data) => {
-       const id = Date.now() + socket.id; // Unique ID
-       projectiles[id] = { ...data, owner: socket.id };
-       io.emit('updateProjectiles', projectiles);
-     });
-
-     // Handle projectile updates (movement)
-     socket.on('updateProjectile', (data) => {
-       if (projectiles[data.id]) {
-         projectiles[data.id] = data;
-         socket.broadcast.emit('updateProjectiles', projectiles);
-       }
-     });
-
-     // Handle disconnect
-     socket.on('disconnect', () => {
-       delete players[socket.id];
-       io.emit('updatePlayers', players);
-     });
-   });
-
-   server.listen(3000, () => {
-     console.log('Server running on http://localhost:3000');
-   });
-   
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
