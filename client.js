093637d7ac901dd41007ba13c37633e38d074697
beformonez <beformonez@gmail.com>
2025-11-22 15:07:28 +0800

    Delete client.js

--- client.js
+++
@@ -1,5 +0,0 @@
-const socket = io("/", { path: "/api/socket" });
-const s = document.getElementById('s');
-
-socket.on("connect", () => { s.textContent = "Connected"; });
-socket.on("pingTest", msg => { console.log("Server:", msg); });
